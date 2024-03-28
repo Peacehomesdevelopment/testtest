e562e2bc1bc99cf96726375385856089984addac
RENBRAN <phdaccounts@peacehomes.ae>
2024-03-28 23:11:03 +0400

    New apps
    New apps

---
+++ eg_product_bulk_publish/__manifest__.py
@@ -0,0 +1,19 @@
+{
+    'name': 'Bulk Publish/Unpublish Product in website',
+    'version': '17.0',
+    'category': 'Purchase',
+    'summery': 'With the use of this addon, the user can publish or unpublish products with single click without switching to the website.',
+    'author': 'INKERP',
+    'website': 'www.inkerp.com',
+    'depends': ['sale', 'website_sale'],
+    
+    'data': [
+        'data/data.xml',
+    ],
+    
+    'images': ['static/description/banner.png'],
+    'license': "OPL-1",
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
