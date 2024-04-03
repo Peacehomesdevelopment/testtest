66a8f58c9425a381de90def2d5b772673e8ea0d4
phddevelopment <155246606+Peacehomesdevelopment@users.noreply.github.com>
2024-04-03 22:31:51 +0400

    Update qr_code_base.py

--- ehcs_qr_code_base/models/qr_code_base.py
+++ ehcs_qr_code_base/models/qr_code_base.py
@@ -1,20 +1,39 @@
-# -*- coding: utf-8 -*-
-import qrcode
 import base64
 from io import BytesIO
+import qrcode
 
+class AccountPayment(models.Model):
+    _inherit = 'account.payment'
 
-def generate_qr_code(url):
-    qr = qrcode.QRCode(
-             version=1,
-             error_correction=qrcode.constants.ERROR_CORRECT_L,
-             box_size=20,
-             border=4,
-             )
-    qr.add_data(url)
-    qr.make(fit=True)
-    img = qr.make_image()
-    temp = BytesIO()
-    img.save(temp, format="PNG")
-    qr_img = base64.b64encode(temp.getvalue())
-    return qr_img
+    qr_code = fields.Binary(string='QR Code', compute='_compute_qr_code', store=True)
+
+    @api.depends('name', 'size')
+    def _compute_qr_code(self):
+        for record in self:
+            base_url = record._generate_base_url()
+            qr_code_img = record._generate_qr_code_image(base_url)
+            record.qr_code = self._convert_image_to_binary(qr_code_img)
+
+    def _generate_base_url(self):
+        # Generate the base URL for the payment
+        base_url = self.env['ir.config_parameter'].get_param('web.base.url')
+        if not 'localhost' in base_url:
+            if 'http://' in base_url:
+                base_url = base_url.replace('http://', 'https://')
+        base_url = base_url + '/web#id=' + str(self.id) + '&model=account.payment&view_type=form&cids='
+        return base_url
+
+    def _generate_qr_code_image(self, base_url):
+        # Generate QR code image
+        qr_code = qrcode.QRCode(version=4, box_size=4, border=1)
+        qr_code.add_data(base_url)
+        qr_code.make(fit=True)
+        qr_img = qr_code.make_image()
+        return qr_img
+
+    def _convert_image_to_binary(self, qr_code_img):
+        # Convert image to binary data
+        buffered = BytesIO()
+        qr_code_img.save(buffered, format="PNG")
+        img_str = base64.b64encode(buffered.getvalue())
+        return img_str
