ef9bc78da6aa16823b36b90c6fdfc16ee1b126ba
RenbranMadelo <phdaccounts@peacehomes.ae>
2024-04-22 14:13:50 +0400

    change

--- formiodata/components/datagrid.py
+++
@@ -1,9 +0,0 @@
-# Copyright Nova Code (http://www.novacode.nl)
-# See LICENSE file for full licensing details.
-
-from .grid_base import baseGridComponent
-
-
-class datagridComponent(baseGridComponent):
-    # XXX should we move the initEmpty from base
-    pass
