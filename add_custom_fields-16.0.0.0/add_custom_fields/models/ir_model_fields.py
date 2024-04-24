4dc27dc7eb5cb4b5a92568345faa97cbe4264b94
phddevelopment <155246606+Peacehomesdevelopment@users.noreply.github.com>
2024-04-24 11:18:58 +0400

    Delete add_custom_fields-16.0.0.0 directory

--- add_custom_fields-16.0.0.0/add_custom_fields/models/ir_model_fields.py
+++
@@ -1,9 +0,0 @@
-from odoo import models, fields
-
-
-class IrModelFields(models.Model):
-    """Adding a new field to understand the dynamically created fields."""
-
-    _inherit = 'ir.model.fields'
-
-    is_dynamic_field = fields.Boolean(string="Dynamic Field")
